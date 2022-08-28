c44b149470b3566bff4f95464b9703423fbd8370
neststack <nsitseo@gmail.com>
2022-08-28 05:54:39 +0530

    making it better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context';
 
 const Navbar = () => {
+  const { setSearchTerm } = useGlobalContext();
+
   return (
     <nav className='navbar'>
       <div className='nav-center'>
-        <Link to='/' className='logoLink'>
+        <Link
+          to='/'
+          className='logoLink'
+          onClick={() => {
+            setSearchTerm('');
+          }}
+        >
           <h2 className='logo'>TheMovieDb</h2>
         </Link>
         <ul className='nav-links'>
           <li>
-            <Link to='/'>Home</Link>
+            <Link
+              to='/'
+              onClick={() => {
+                setSearchTerm('');
+              }}
+            >
+              Home
+            </Link>
           </li>
           <li>
             <Link to='/about'>About</Link>
